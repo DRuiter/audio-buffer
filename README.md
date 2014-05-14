a26dcd8c41570d08bdfdf5362173b4e54456aa51
DRuiter <d.ruiter@live.nl>
2014-05-14 16:03:04 +0200

    First Commit
    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 audio-buffer
 ============
+
+A buffer for Node that accepts PCM Samples and converts these into FFT Samples.
+
+#Note
+
+Currently not published on npm (still very much in development), I'll publish it after some further testing.
+
+##Dependencies
+- timed-buffer
+- fft (currently using https://gist.github.com/mohayonao/3063634)
+
+#Usage
+
+##Instantiation
+AudioBuffer takes 2 parameters.
+
+- timeMS: length of the buffer in miliseconds.
+- options: optional options object.
+	- options.sampleRate: sample rate of the incoming PCM samples. (Default: 44100)
+	- options.fftSize: size of the FFT (Default: 1024)
+
+It also passes it's options object along to the constructor for the TimedBuffer (see: https://github.com/DRuiter/timed-buffer)
+
+##Methods
+
+###getFrequencyData
+Accepts a PCM Sample and returns an FFT Sample.
+
+###condenseSample
+Accepts an FFTSample and a condensationFactor (int). An FFT Sample with 512 bins
+and a condensationfactor of 2 will return an FFT Sample with 256 bins, averaging the
+db over the condensed bins.
+
+###calculateHistory
+Accepts an FFT Sample and a time in miliseconds. Appends the following data to the FFT Sample
+(for each bin) based on the requested history.
+
+- mean
+- mean difference
+- variance
+- standard deviation
+
+###getLast
+Accepts an optional options object with the following parameters:
+
+- condensationFactor: (int) amount with which to condense the FFT Sample (see condenseSample)
+
+###getSample
+Accepts a time in miliseconds for creating an average FFT Sample (see toSample) and an optional options object with the following parameters:
+
+- addHistory: (bool) adds history to the sample .
+- condensationFactor: (int) amount with which to condense the FFT Sample (see condenseSample)
+
+###toSample
+Accepts an Array of FFT Samples, returns 1 FFT Sample that is the average of all supplied
+FFT Samples.
+
+###getFrequencyRange
+Accepts an FFT Sample a frequency range to start at (int) and a frequency range to end at (int).
+Returns an FFT Sample that is filtered to containt only frequency ranges >= start and <= end.
+
+###push
+Accepts a PCM Sample and pushes these to the internal PCM and FFT buffers.
---
+++ audio-buffer.js
@@ -0,0 +1,264 @@
+var	FFT				= require("./fft"),
+	TimedBuffer 	= require('./timed-buffer');
+
+function AudioBuffer (timeMS, options){
+	if(typeof(options) !== 'object') options = {};
+	
+	var self = this;
+
+	this.sampleRate = options.sampleRate || 44100;
+	this.fftSize 	= options.fftSize || 1024;
+	this.binSize	= Math.floor(this.sampleRate/this.fftSize);
+
+	this.fft 		= new FFT(this.fftSize);
+	this.PCM 		= new TimedBuffer(timeMS, options);
+	this.FFT 		= new TimedBuffer(timeMS, options);
+
+	this.getFrequencyData = function ( PCMSample ){
+		var fourier			= this.fft.forward(PCMSample),
+			frequencyData 	= [];
+
+		for(var i = 0, l = fourier.real.length; i < l; i++){
+			var real 		= fourier.real[i],
+				imag 		= fourier.imag[i],
+				magnitude 	= Math.sqrt((real*real)+(imag*imag));
+
+			frequencyData.push(magnitude);
+		}
+
+		return frequencyData
+			.slice(0, (this.fftSize/2)+1)
+			.map(function (item, index){
+				var sampleInfo = {
+					freq 		: Math.round((index * self.sampleRate/2)/(self.fftSize/2)),
+					db	 		: Math.round((20 * (Math.log(item) / Math.LN10))+100)
+				};
+
+				return sampleInfo;
+			});
+	}
+
+	this.condenseSample = function (FFTSample, factor){
+		var condensed 	= [
+				[]
+			],
+			hasHistory	= false,
+			iterator	= 0;
+
+		if(FFTSample[0].mean !== 'undefined') hasHistory = true;
+
+		function averageArray(array){
+			return array.reduce(function (prev, cur, index, array){
+				if(index === array.length-1){
+					return Math.round((prev+cur)/array.length);
+				} else {
+					return prev+cur;
+				}
+			})
+		}
+
+		if(hasHistory){
+
+			FFTSample.forEach(function (item, index){
+				if(index === 0) {
+					condensed[iterator] = {
+						db 					: [],
+						mean 				: [],
+						meanDiff 			: [],
+						variance 			: [],
+						standardDeviation 	: []
+					}
+				}
+
+				if(index !== 0 && index%factor === 0) {
+					condensed[iterator].db = averageArray(condensed[iterator].db);
+					condensed[iterator].mean = averageArray(condensed[iterator].mean);
+					condensed[iterator].meanDiff = averageArray(condensed[iterator].meanDiff);
+					condensed[iterator].variance = averageArray(condensed[iterator].variance);
+					condensed[iterator].standardDeviation = averageArray(condensed[iterator].standardDeviation);
+
+					condensed.push({
+						db 					: [],
+						mean 				: [],
+						meanDiff 			: [],
+						variance 			: [],
+						standardDeviation 	: []
+					});
+
+					iterator++;
+				}
+
+				condensed[iterator].db.push(item.db);
+				condensed[iterator].mean.push(item.mean);
+				condensed[iterator].meanDiff.push(item.meanDiff);
+				condensed[iterator].variance.push(item.variance);
+				condensed[iterator].standardDeviation.push(item.standardDeviation);
+			})
+
+			return condensed.map(function (item, index, array){
+				var times = index+1;
+
+				item.freq = [(self.binSize*factor*index), (self.binSize*factor*times)];
+
+				return item;
+			})
+
+		} else {
+
+			FFTSample.forEach(function (item, index){
+				if(index !== 0 && index%factor === 0) {
+					condensed[iterator] = averageArray(condensed[iterator]);
+					condensed.push([]);
+					iterator++;
+				}
+
+				condensed[iterator].push(item.db);
+			})
+
+			return condensed.map(function (item, index, array){
+				var times = index+1;
+
+				return {
+					db 		: item,
+					freq	: [(self.binSize*factor*index), (self.binSize*factor*times)]
+				}
+			})
+		}
+	}
+
+	this.averageSample = function ( FFTArray ){
+		var average = [];
+
+		FFTArray.forEach(function (array){
+			array.forEach(function (item, index){
+				if(!average[index])
+					average[index] = item;
+				else 
+					average[index].db += item.db;
+			});
+		});
+
+		return average.map(function (item){
+			item.db = item.db/FFTArray.length;
+			return item;
+		})
+	}
+
+	this.calculateHistory = function ( FFTSample, timeMS ){
+		if(!timeMS) timeMS = 500;
+		
+		var compare 			= this.FFT.getByTime(timeMS),
+			sortedByBin 		= [],
+			mean,
+			meanDiff,
+			variance,
+			standardDeviation;
+
+		FFTSample.forEach(function(){
+			sortedByBin.push([]);
+		})
+
+		compare.forEach(function (sample, index){
+			sample.forEach(function (item, innerIndex){
+				sortedByBin[innerIndex].push(item.db);
+			})
+		})
+
+		mean = sortedByBin.map(function (item, index){
+			return item.reduce(function (prev, cur, innerIndex, array){
+				if(innerIndex === array.length-1)
+					return (prev+cur)/array.length;
+				else
+					return prev+cur;
+			});
+		});
+
+		meanDiff = sortedByBin.map(function (item, index){
+			return item.map(function (innerItem, innerIndex){
+				return innerItem-mean[index];
+			});
+		})
+
+		variance = meanDiff.map(function (item, index){
+			return item.reduce(function (prev, cur, innerIndex, array){
+				if(innerIndex === array.length-1)
+					return (prev+Math.pow(cur, 2))/array.length;
+				else
+					return prev+Math.pow(cur, 2);
+			})
+		})
+
+		standardDeviation = variance.map(function (item, index){
+			return Math.round(Math.sqrt(item, 2));
+		})
+
+		return FFTSample.map(function (item, index){
+			item.mean 				= mean[index];
+			item.meanDiff			= item.db-mean[index];
+			item.variance			= variance[index];
+			item.standardDeviation 	= standardDeviation[index];
+
+			return item;
+		})
+	}
+
+	this.getLast		= function (options){
+		if(options == null) options = {};
+
+		options.condensationFactor = options.condensationFactor || 1;
+
+		var sample = this.FFT.buffer[this.FFT.buffer.length-1];
+
+		return this.condenseSample(sample, options.condensationFactor);
+	}
+
+	this.getSample		= function (timeMS, options){
+		if(typeof(options) !== 'object') options = {};
+
+		var sample;
+
+		if(options.addHistory)
+			sample = this.calculateHistory(this.toSample(this.FFT.getByTime(timeMS)), 1000);
+		else
+			sample = this.toSample(this.FFT.getByTime(timeMS));
+
+		if(options.condensationFactor && typeof(options.condensationFactor) === 'number')
+			return this.condenseSample(sample, options.condensationFactor);
+		else
+			return sample;
+	}
+
+	this.toSample = function( FFTArray ){
+		var sample = [];
+
+		FFTArray.forEach(function (FFTSample, index){
+			FFTSample.forEach(function (item, i){
+				if(typeof(sample[i]) !== 'object')
+					sample[i] = item;
+				else 
+					sample[i].db = sample[i].db+item.db;
+			})
+		})
+
+		return sample.map(function (item){
+			item.db = Math.round(item.db/FFTArray.length);
+
+			return item;
+		})
+	}
+
+	this.getFrequencyRange = function (FFTSample, start, end){
+		return FFTSample.filter(function (item){
+			if(item.freq >= start && item.freq <= end) return item;
+		})
+	}
+
+	this.push = function ( PCMSample ) {
+		this.PCM.push(PCMSample);
+		this.FFT.push(this.getFrequencyData(PCMSample));	
+	}
+
+	return this;
+}
+
+module.exports = AudioBuffer;
